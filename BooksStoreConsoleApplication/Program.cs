9dbc10773a84a0caa8661531bcc286c1b390b7c2
Prarup Gurung <grgprarup@gmail.com>
2023-03-29 08:56:36 +0545

    Array declaration and initialization

--- BooksStoreConsoleApplication/Program.cs
+++ BooksStoreConsoleApplication/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,8 +13,8 @@ namespace BooksStoreConsoleApplication
         // this is an entry point of the program
         static void Main(string[] args)
         {
-
-            // Lets create a object of Books class
+            /* [multiline comments]
+            // Lets create a object of Books class [single line comment]
             Books book1 = new Books();
 
             // Lets assign values to the object using properties (setters)
@@ -21,18 +22,38 @@ namespace BooksStoreConsoleApplication
             book1.Name = "C# Programming";
             book1.Author = "Harry Daniel";
             book1.Publisher = "MacGraw Hill";
+            book1.PublishedYears = new int[4] { 2000,2002,2010,2022};
+            */
+
+            // Simplified version of object creation with initialization
+            Books book1 = new Books()
+            {
+                ISBN = 456,
+                Name = "C# Programming",
+                Author = "Harry Daniel",
+                Publisher = "MacGraw Hill",
+                PublishedYears = new int[4] { 2000, 2002, 2010, 2022 }
+            };
 
             // Lets print the values using method
             book1.printBooksDetails();
 
             // Lets create a object of Books class and assign values using constructor
-            Books book2 = new Books(123, ".Net Programming", "Robin Crook", "Graham Bell Publishing House");
+            Books book2 = new Books(123, ".Net Programming", "Robin Crook", "Graham Bell Publishing House",new int[7] {1992,1995,2000,2005,2010,2015,2020});
 
             // Lets print the values using properties (getters)
+            // WriteLine prints single statement and adds new line
+            Console.WriteLine();
             Console.WriteLine("ISBN : {0}", book2.ISBN);
             Console.WriteLine("Name: {0}", book2.Name);
             Console.WriteLine("Author: {0}", book2.Author);
             Console.WriteLine("Publisher: {0}", book2.Publisher);
+            // Write prints single statement and does not add new line
+            Console.Write("Published Years: ");
+            foreach (int publishedYear in  book2.PublishedYears)
+            {
+                Console.Write("{0}, ", publishedYear);
+            }
         }
     }
 }
