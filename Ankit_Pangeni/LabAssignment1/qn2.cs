88af254cb72d254bf600c0322a112c42bdae45a6
ankit <ankitpangeni74@gmail.com>
2023-04-11 06:37:41 +0545

    1st and 2nd question

---
+++ Ankit_Pangeni/LabAssignment1/qn2.cs
@@ -0,0 +1,19 @@
+using System;
+
+class LeapYearChecker
+{
+    static void Main()
+    {
+        Console.Write("Enter a year: ");
+        int year = int.Parse(Console.ReadLine());
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+        {
+            Console.WriteLine("{0} is a leap year.", year);
+        }
+        else
+        {
+            Console.WriteLine("{0} is not a leap year.", year);
+        }
+    }
+}
