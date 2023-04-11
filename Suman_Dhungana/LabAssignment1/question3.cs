06d16a52d51ac47bf9d66b70b8545d74b78c34ca
Suman Dhungana <dhunganasuman226@gmail.com>
2023-04-11 15:42:32 +0545

    first commit

---
+++ Suman_Dhungana/LabAssignment1/question3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your age: ");
+        int age = int.Parse(Console.ReadLine());
+
+        if (age >= 18)
+        {
+            Console.WriteLine("You can get citizenship and voter id");
+        }
+        else if (age >= 16)
+        {
+            Console.WriteLine("You can get citizenship but cannot voter id");
+        }
+        else
+        {
+            Console.WriteLine("You cannot get citizenship");
+        }
+    }
+}
---
+++ Suman_Dhungana/LabAssignment1/question4.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+class Vehicle
+{
+    public double distance;
+    public double fuel;
+    public int seats;
+    public string action;
+
+    public Vehicle(double distance, double fuel, int seats, string action)
+    {
+        this.distance = distance;
+        this.fuel = fuel;
+        this.seats = seats;
+        this.action = action;
+    }
+
+    public double KilometersPerLiter()
+    {
+        return distance / fuel;
+    }
+
+    public double FuelNeededFor100Km()
+    {
+        return 100 / KilometersPerLiter();
+    }
+
+    public void Steering(string direction)
+    {
+        action = "rotate steering wheel " + direction;
+    }
+}
+
+class Motorcycle : Vehicle
+{
+    public Motorcycle(double distance, double fuel,int seats, string action)
+        : base(distance, fuel, seats, action)
+    {
+    }
+
+    public new void Steering(string direction)
+    {
+        action = "lean over " + direction;
+    }
+}
+
+class Car : Vehicle
+{
+    public Car(double distance, double fuel,int seats, string action)
+        : base(distance, fuel,seats, action)
+    {
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("---------for MOtorcycle--------");
+        Motorcycle myMotorcycle = new Motorcycle(300, 10,2, "lean over straight");
+        Console.WriteLine("Kilometers per liter: " + myMotorcycle.KilometersPerLiter());
+        Console.WriteLine("Fuel needed for 100 km: " + myMotorcycle.FuelNeededFor100Km());
+        myMotorcycle.Steering("right");
+        Console.WriteLine("Action to steer: " + myMotorcycle.action);
+
+        Console.WriteLine("\r\n---------for Car--------");
+        Car myCar = new Car(800, 40,5, "rotate steering wheel straight");
+        Console.WriteLine("Kilometers per liter: " + myCar.KilometersPerLiter());
+        Console.WriteLine("Fuel needed for 100 km: " + myCar.FuelNeededFor100Km());
+        myCar.Steering("right");
+        Console.WriteLine("Action to steer: " + myCar.action);
+    }
+}
