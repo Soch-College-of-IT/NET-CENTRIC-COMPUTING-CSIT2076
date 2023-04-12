bc8de3823447d1732000a976ab9bbc91b5306c75
ganeshsubedi111 <subediganesh306@gmail.com>
2023-04-12 08:02:36 +0545

    Ganesh Lab Assignment

---
+++ Ganesh_Subedi/Lab_Assignment1/qno-2.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace LeapYear
+{
+    class Leap
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+        {
+            Console.WriteLine("{0} is a leap year.", year);
+        }
+        else
+        {
+            Console.WriteLine("{0} is not a leap year.", year);
+        }
+      }
+      
+    }
+}
