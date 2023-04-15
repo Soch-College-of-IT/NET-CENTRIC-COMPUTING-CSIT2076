3b8d051a9ab1430de34fbfa185309d2e174f963d
dyells07 <bhimkhanal999@gmail.com>
2023-04-15 23:35:14 +0545

    Bipin Khanal Lab Assignment1

---
+++ Bipin_Khanal/Citizenship.cs
@@ -0,0 +1,60 @@
+﻿// using System;
+
+// namespace Bipin_Khanal
+// {
+//     class Program
+//     {
+//         // Main method
+//         static void Main(string[] args)
+//         {
+//             // for taking input from user
+//             Console.Write("Enter age: ");
+//             // for converting string to integer
+//             int age = Convert.ToInt32(Console.ReadLine());
+
+//             // check if age is greater than 100
+//             if (age > 100)
+// {
+//     // check if age is greater than 100 and alive
+//     Console.Write("Are you Human? (Y/N): ");
+//     string response = Console.ReadLine();
+
+//     if (response.ToUpper() == "Y")
+//     {
+//         Console.WriteLine("You must be a very healthy person!");
+//         if (age >= 18)
+//         {
+//             Console.WriteLine("You can get citizenship and vote");
+//         }
+//         else if (age >= 16)
+//         {
+//             Console.WriteLine("You can get citizenship but cannot vote");
+//         }
+//         else
+//         {
+//             Console.WriteLine("You cannot get citizenship");
+//         }
+//     }
+//     else
+//     {//sorry only human can vote and get citizenship
+//         Console.WriteLine("Sorry only human can vote and get citizenship .");
+//     }
+// }
+// else if (age >= 18)
+// {
+//     Console.WriteLine("You can get citizenship and vote");
+// }
+// else if (age >= 16)
+// {
+//     Console.WriteLine("You can get citizenship but cannot vote");
+// }
+// else
+// {
+//     Console.WriteLine("You cannot get citizenship");
+// }
+
+
+//             Console.ReadLine();
+//         }
+//     }
+// }
