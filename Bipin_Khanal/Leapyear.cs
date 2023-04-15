742ad382f369838e8a65d59c4e2721f6a81f2b8b
Bipin Khanal <76544001+dyells07@users.noreply.github.com>
2023-04-15 23:41:58 +0545

    Update Leapyear.cs

--- Bipin_Khanal/Leapyear.cs
+++ Bipin_Khanal/Leapyear.cs
@@ -1,20 +1,19 @@
-﻿
 
-// using System;
+ using System;
 
-// class Program {
-//     static void Main(string[] args) {
-//         //for taking input from user
-//         Console.Write("Enter a year: ");
-//         int year = int.Parse(Console.ReadLine());
-//         //for checking if the year is leap year or not
-//         if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
-//             Console.WriteLine(year + " is a leap year.");
-//         }
-//         else {
-//             Console.WriteLine(year + " is not a leap year.");
-//         }
-//         //for pausing the console
-//         Console.ReadLine();
-//     }
-// }
+ class Leapyear {
+     static void Main(string[] args) {
+         //for taking input from user
+       Console.Write("Enter a year: ");
+        int year = int.Parse(Console.ReadLine());
+        //for checking if the year is leap year or not
+         if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+            Console.WriteLine(year + " is a leap year.");
+       }
+        else {
+          Console.WriteLine(year + " is not a leap year.");
+       }
+      //for pausing the console
+        Console.ReadLine();
+     }
+}
