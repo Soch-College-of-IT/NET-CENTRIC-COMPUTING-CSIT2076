cf0a83da334d33c3bf90be554b871017776cba5e
Kaiisenseii <kaiisenseii465@gmail.com>
2023-04-18 14:43:14 +0545

    added all question

---
+++ Kabir_Shrestha/LabAssignment1/question2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class leapyear
+{
+    static void Main(string[] args)
+    {
+        int year;
+        Console.Write("Enter the year: ");
+        string input = Console.ReadLine();
+        year = Convert.ToInt32(input);
+
+        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+        {
+            Console.WriteLine("{0} is a leap year.", year);
+        }
+        else
+        {
+            Console.WriteLine("{0} is not a leap year.", year);
+        }
+    }
+}
