975494396afb265035e0dc7dec2da13fcf0b0a4e
Prarup Gurung <grgprarup@gmail.com>
2023-04-20 22:23:20 +0545

    MVC Project Setup

---
+++ StudentInformationSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentInformationSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
