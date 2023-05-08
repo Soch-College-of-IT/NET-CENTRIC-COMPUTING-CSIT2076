bc6e50a8b4fc3173ec102e93965ecb562084f109
Prarup Gurung <grgprarup@gmail.com>
2023-05-08 23:16:04 +0545

    Added Data Annotations

--- ModelBindingDemo/Models/UserViewModel.cs
+++ ModelBindingDemo/Models/UserViewModel.cs
@@ -4,7 +4,9 @@ namespace ModelBindingDemo.Models
 {
     public class UserViewModel
     {
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
     }
 }
