1da07fcb4fce388d15cd66951d8101bf36f3d2f5
Prarup Gurung <grgprarup@gmail.com>
2023-05-08 21:48:20 +0545

    Added UserController

---
+++ ModelBindingDemo/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelBindingDemo.Models;
+
+namespace ModelBindingDemo.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public IActionResult SimpleBinding()
+        {
+            return View(new WebUser() { FirstName = "Alice", LastName = "Hansen"});
+        }
+    }
+}
