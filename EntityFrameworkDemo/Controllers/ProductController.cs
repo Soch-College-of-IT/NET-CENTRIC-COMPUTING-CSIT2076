3a9d8356740640b867423b57e4b0dfb84485763e
Prarup Gurung <grgprarup@gmail.com>
2023-06-02 08:49:06 +0545

    Entity Framework Project

---
+++ EntityFrameworkDemo/Controllers/ProductController.cs
@@ -0,0 +1,191 @@
+﻿using EntityFrameworkDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace EntityFrameworkDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Product()
+        {
+            List productList = new List();
+            var dbconfig = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json").Build();
+            try
+            {
+                string dbconnectionStr = dbconfig["ConnectionStrings:DefaultConnection"];
+                string sql = "SP_Get_ProductList";
+                using (SqlConnection connection = new SqlConnection(dbconnectionStr))
+                {
+                    SqlCommand command = new SqlCommand(sql, connection);
+                    using (SqlDataReader dataReader = command.ExecuteReader())
+                    {
+                        while (dataReader.Read())
+                        {
+                            Product product = new Product();
+                            product.Id = Convert.ToInt32(dataReader["Id"]);
+                            product.ProductName = Convert.ToString(dataReader["ProductName"]);
+                            product.ProductDescription = Convert.ToString(dataReader["ProductDescription"]);
+                            product.ProductCost = Convert.ToDecimal(dataReader["ProductCost"]);
+                            product.Stock = Convert.ToInt32(dataReader["Stock"]);
+                            productList.Add(product);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return View(productList);
+        }
+
+        public IActionResult ProductCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ProductCreate(Product product)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var dbconfig = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json").Build();
+                    if (!string.IsNullOrEmpty(dbconfig.ToString()))
+                    {
+                        string dbconnectionStr = dbconfig["ConnectionStrings:DefaultConnection"];
+                        using (SqlConnection connection = new SqlConnection(dbconnectionStr))
+                        {
+                            string sql = "SP_Add_New_Product";
+                            using (SqlCommand cmd = new SqlCommand(sql, connection))
+                            {
+                                cmd.CommandType = CommandType.StoredProcedure;
+                                cmd.Parameters.AddWithValue("@ProductName", product.ProductName);
+                                cmd.Parameters.AddWithValue("@ProductDescription", product.ProductDescription);
+                                cmd.Parameters.AddWithValue("@ProductCost", product.ProductCost);
+                                cmd.Parameters.AddWithValue("@Stock", product.Stock);
+                                connection.Open();
+                                cmd.ExecuteNonQuery();
+                                connection.Close();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return RedirectToAction("Product");
+        }
+
+        public IActionResult ProductUpdate(int id)
+        {
+            var dbconfig = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json").Build();
+            string dbconnectionStr = dbconfig["ConnectionStrings:DefaultConnection"];
+            Product product = new Product();
+            using (SqlConnection connection = new SqlConnection(dbconnectionStr))
+            {
+                string sql = "SP_Get_Product_By_Id";
+                using (SqlCommand cmd = new SqlCommand(sql, connection))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@id", id);
+                    connection.Open();
+                    using (SqlDataReader dataReader = cmd.ExecuteReader())
+                    {
+                        while (dataReader.Read())
+                        {
+                            product.Id = Convert.ToInt32(dataReader["Id"]);
+                            product.ProductName = Convert.ToString(dataReader["ProductName"]);
+                            product.ProductDescription = Convert.ToString(dataReader["ProductDescription"]);
+                            product.ProductCost = Convert.ToDecimal(dataReader["ProductCost"]);
+                            product.Stock = Convert.ToInt32(dataReader["Stock"]);
+                        }
+                    }
+                }
+                connection.Close();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult ProductUpdate(Product product)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var dbconfig = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json").Build();
+                    if (!string.IsNullOrEmpty(dbconfig.ToString()))
+                    {
+                        string dbconnectionStr = dbconfig["ConnectionStrings:DefaultConnection"];
+                        using (SqlConnection connection = new SqlConnection(dbconnectionStr))
+                        {
+                            string sql = "SP_Update_Product";
+                            using (SqlCommand cmd = new SqlCommand(sql, connection))
+                            {
+                                cmd.CommandType = CommandType.StoredProcedure;
+                                cmd.Parameters.AddWithValue("@Id", product.Id);
+                                cmd.Parameters.AddWithValue("@ProductName", product.ProductName);
+                                cmd.Parameters.AddWithValue("@Stock", product.Stock);
+                                connection.Open();
+                                cmd.ExecuteNonQuery();
+                                connection.Close();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return RedirectToAction("Product");
+        }
+
+        public IActionResult ProductDelete(int id)
+        {
+            var dbconfig = new ConfigurationBuilder()
+                       .SetBasePath(Directory.GetCurrentDirectory())
+                       .AddJsonFile("appsettings.json").Build();
+
+            string dbconnectionStr = dbconfig["ConnectionStrings:DefaultConnection"];
+            using (SqlConnection connection = new SqlConnection(dbconnectionStr))
+            {
+                string sql = "SP_Delete_Product_By_Id";
+                using (SqlCommand cmd = new SqlCommand(sql, connection))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Id", id);
+                    connection.Open();
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (SqlException ex)
+                    {
+                        throw;
+                    }
+                    connection.Close();
+                }
+            }
+            return RedirectToAction("Product");
+        }
+    }
+}
---
+++ EntityFrameworkDemo/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace EntityFrameworkDemo.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public decimal ProductCost { get; set; }
+        public int Stock { get; set; }
+    }
+}
