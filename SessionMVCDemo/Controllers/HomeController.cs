60b4d34c1e1bb5c2c48b2c111596b81f550b9c91
Prarup Gurung <grgprarup@gmail.com>
2023-06-06 01:08:53 +0545

    Session demo project

---
+++ SessionMVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SessionMVCDemo.Models;
+using System.Diagnostics;
+
+namespace SessionMVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            HttpContext.Session.SetString("UserName", "John Doe");
+            return RedirectToAction("Welcome");
+        }
+
+        public IActionResult Welcome()
+        {
+            string userName = HttpContext.Session.GetString("UserName");
+            ViewData["UserName"] = userName;
+            return View();
+        }
+
+        public IActionResult ClearSession()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Welcome");
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
